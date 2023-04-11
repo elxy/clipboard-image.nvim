49cd68dd5fd71aa293c9e66e0339065eeec0ef52
elxy <elxy@outlook.com>
2023-04-11 15:31:09 +0800

    fix: escape shell expansion in WSL

--- lua/clipboard-image/utils.lua
+++ lua/clipboard-image/utils.lua
@@ -34,9 +34,9 @@ M.get_clip_command = function()
     cmd_paste = "pngpaste '%s'"
   elseif this_os == "Windows" or this_os == "Wsl" then
     cmd_check = "Get-Clipboard -Format Image"
-    cmd_paste = "$content = " .. cmd_check .. ";$content.Save('%s', 'png')"
-    cmd_check = 'powershell.exe "' .. cmd_check .. '"'
-    cmd_paste = 'powershell.exe "' .. cmd_paste .. '"'
+    cmd_paste = '$content = ' .. cmd_check .. ';$content.Save("%s", "png")'
+    cmd_check = "powershell.exe '" .. cmd_check .. "'"
+    cmd_paste = "powershell.exe '" .. cmd_paste .. "'"
   end
   return cmd_check, cmd_paste
 end
